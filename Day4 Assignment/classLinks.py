819344a73d36f10b80b726258c2f08043c8d4186
tushantkumar <72964792+tushantkumar@users.noreply.github.com>
2020-12-06 22:46:21 +0530

    Update classLinks.py

--- Day4 Assignment/classLinks.py
+++ Day4 Assignment/classLinks.py
@@ -1,16 +1,26 @@
-class Program:
-    def __init__(self, file_name):
-        self.t = open(file_name, 'r')
+import re
 
-    def check(self):
-        ...
+class LinkClass:
+    def __init__(self, name, link, time):
+        self.name = name
+        self.link = link
+        self.time = time
 
-    def close(self):
-        if self.t:
-            self.t.close()
-            self.t = None
+urls = []
+names = []
+times = []
+with open('D:\\Sify documents\\Python training\\Assignment\\Day4 Assignment\\links.txt', 'r') as txt_file:
+    data = txt_file.read()
+    urls = re.findall(r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', data)
+    names = re.findall("(?<=from )(.*)(?=to)", data)
+    times = re.findall("[0-1][0-2]:[0-5][0-9] AM|[0-1][0-2]:[0-5][0-9] PM", data)
 
 
-import contextlib
-with contextlib.closing(Program('D:\\Sify documents\\Python training\\Assignment\\Day4 Assignment\\links.txt')) as program:
-    program.check()
+list_links = []
+
+for (a,b,c) in zip(names, urls, times):
+    l = LinkClass(a,b,c)
+    list_links.append(l)
+
+for link in list_links:
+    print(f'Sent By: {link.name} \n Link: {link.link} \n At: {link.time}')
